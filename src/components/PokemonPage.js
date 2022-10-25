387c0f1cead2b71631f26d7e39a592a65fa258ab
Cesar Hernandez <chema27281@gmail.com>
2022-10-25 02:38:14 -0400

    updated json

---
+++ src/components/PokemonPage.js
@@ -0,0 +1,13 @@
+import PokemonList from "./PokemonList";
+import PokemonForm from "./PokemonForm";
+
+function PokemonPage() {
+  return (
+    <>
+      <PokemonList />
+      <PokemonForm />
+    </>
+  );
+}
+
+export default PokemonPage;
