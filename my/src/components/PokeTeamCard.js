1b5594f313741b9b72c0c2ad00e95ca445053044
Cesar Hernandez <chema27281@gmail.com>
2022-10-27 14:27:18 -0400

    sliders

--- my/src/components/PokeTeamCard.js
+++ my/src/components/PokeTeamCard.js
@@ -1,4 +1,10 @@
 function PokeTeamCard({ team, onDelete }) {
+  const [hpEvs, setHpEvs] = useState('')
+  const [atkEvs, setAtkEvs] = useState('')
+  const [defenseEvs, setDefenseEvs] = useState('')
+  const [spAtkEvs, setSpAtkEvs] = useState('')
+  const [spDefenseEvs, setSpDefenseEvs] = useState('')
+  const [speedEvs, setSpeedEvs] = useState('')
   function handleDelete() {
     fetch(`http://localhost:4000/PokeTeam/${team.id}`, {
       method: "DELETE",
@@ -15,13 +21,50 @@ function PokeTeamCard({ team, onDelete }) {
         </div>
         <div className="statsBar">
           Pokémon Stats
-          <p>HP:{team.hp}</p>
-          <p>Attack: {team.attack}</p>
-          <p>Defense: {team.defense}</p>
-          <p>Special attack: {team["spattack"]}</p>
-          <p>Special defense: {team["spdefense"]}</p>
-          <p>Speed: {team.speed}</p>
-          <p>Nature: {team.nature}</p>
+          <p>HP:{team.hp}
+            <input type="number" id="ev-hp-number" min="0" max="252" step="4"
+              value={hpEvs} onChange={e => setHpEvs(e.target.value)}>
+            </input>
+            <input type="range" id="ev-hp-range" min="0" max="252" step="4" value={hpEvs} onChange={e => setHpEvs(e.target.value)}>
+            </input>
+
+          </p>
+          <p>Attack: {team.attack}
+            <input type="number" id="ev-hp-number" min="0" max="252" step="4"
+              value={atkEvs} onChange={e => setAtkEvs(e.target.value)}>
+            </input>
+            <input type="range" id="ev-hp-range" min="0" max="252" step="4" value={atkEvs} onChange={e => setAtkEvs(e.target.value)}>
+            </input>
+          </p>
+          <p>Defense: {team.defense}
+            <input type="number" id="ev-hp-number" min="0" max="252" step="4"
+              value={defenseEvs} onChange={e => setDefenseEvs(e.target.value)}>
+            </input>
+            <input type="range" id="ev-hp-range" min="0" max="252" step="4" value={defenseEvs} onChange={e => setDefenseEvs(e.target.value)}>
+            </input>
+          </p>
+          <p>Special attack: {team["spattack"]}
+            <input type="number" id="ev-hp-number" min="0" max="252" step="4"
+              value={spAtkEvs} onChange={e => setSpAtkEvs(e.target.value)}>
+            </input>
+            <input type="range" id="ev-hp-range" min="0" max="252" step="4" value={spAtkEvs} onChange={e => setSpAtkEvs(e.target.value)}>
+            </input>
+          </p>
+          <p>Special defense: {team["spdefense"]}
+            <input type="number" id="ev-hp-number" min="0" max="252" step="4"
+              value={spDefenseEvs} onChange={e => setSpDefenseEvs(e.target.value)}>
+            </input>
+            <input type="range" id="ev-hp-range" min="0" max="252" step="4" value={spDefenseEvs} onChange={e => setSpDefenseEvs(e.target.value)}>
+            </input>
+          </p>
+          <p>Speed: {team.speed}
+            <input type="number" id="ev-hp-number" min="0" max="252" step="4"
+              value={speedEvs} onChange={e => setSpeedEvs(e.target.value)}>
+            </input>
+            <input type="range" id="ev-hp-range" min="0" max="252" step="4" value={speedEvs} onChange={e => setSpeedEvs(e.target.value)}>
+            </input>
+          </p>
+
         </div>
       </div>
       <button className="add"> Edit</button>
