e817a8f874a7862b8d2724687554f30ccd23ea1f
Dulat <dulat.kussainov@gmail.com>
2022-10-26 20:04:53 -0400

    PokeDuel update

--- my/src/components/PokemonPage.js
+++ my/src/components/PokemonPage.js
@@ -3,6 +3,7 @@ import PokemonTeam from "./PokemonTeam";
 import PokeDuel from "./PokeDuel";
 import React, { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import Draggable from "react-draggable";
 
 function PokemonPage({ pokemons, setPokiSearch, pokiSearch }) {
   const [teams, setTeams] = useState([]);
@@ -38,10 +39,28 @@ function PokemonPage({ pokemons, setPokiSearch, pokiSearch }) {
           />
         </Route>
         <Route exact patch="/pokeDuel">
-          <PokeDuel />
+          <PokeDuel teams={teams} pokemons={pokemons} />
         </Route>
       </Switch>
+      <Draggable>
+        <div className="box">
+          <img
+            className="pokeball"
+            src="https://i.pinimg.com/originals/32/eb/23/32eb230b326ee3c76e64f619a06f6ebb.png"
+            alt=""
+          />
+        </div>
+      </Draggable>
+      <div className="toTheTop">
+        <a href="Header">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYwb1zSxXjhhXFc5Wii5bb6NdEo4-rzfMshQ&usqp=CAU"
+            alt=""
+          />
+        </a>
+      </div>
     </div>
+    
   );
 }
 
